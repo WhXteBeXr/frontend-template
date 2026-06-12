cf77c34cd8d3a49cac3529fe348947c46a3f47c6
WhXteBeXr <WhiteBear_LT@outlook.com>
2026-06-12 18:42:11 +0300

    deploy config for GitHub pages added

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'node:url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: '/repo-name/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
